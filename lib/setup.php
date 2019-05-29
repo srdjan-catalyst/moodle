5b5d5e248ee9afe499a66cd281f47fd48cd6e9aa
Michael Palimaka <michaelpalimaka@catalyst-au.net>
2019-05-29 11:56:35 +1000

    WR#309195: send custom HTTP header when in maintenance mode

--- lib/setup.php
+++ lib/setup.php
@@ -333,6 +333,7 @@ if (file_exists("$CFG->dataroot/climaintenance.html")) {
         header('Expires: Mon, 20 Aug 1969 09:23:00 GMT');
         header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
         header('Accept-Ranges: none');
+        header('X-Moodle-Maintenance: cli');
         readfile("$CFG->dataroot/climaintenance.html");
         die;
     } else {
